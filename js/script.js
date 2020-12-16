237c5e6ca2fc9cc48697ec79b349ca27dd8f8deb
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-16 15:38:11 +0100

    Es Finito

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var newCognome = prompt('Inserisci il tuo cognome');
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var outputCognomi = document.getElementById('outputCognomi');
+listaCognomi.push(newCognome);
+listaCognomi.sort();
+
+var contenutoPrecedente;
+
+for (var i = 0; i < listaCognomi.length; i++) {
+
+  contenutoPrecedente = outputCognomi.innerHTML;
+  outputCognomi.innerHTML = contenutoPrecedente + '<li>' + listaCognomi[i] + '</li>';}
+
+  document.getElementById('posizione').innerText = 'La posizione é:' + listaCognomi.indexOf(newCognome);
